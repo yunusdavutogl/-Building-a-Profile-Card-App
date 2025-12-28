7b01ef13717104a8bd75e18574ec4bc79e1214b2
yunus davutoğlu <210408042@st.biruni.edu.tr>
2025-12-28 23:53:20 +0300

    Part 1: Project setup and initial styling complete

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text>My Profile App</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f0f2f5', // Açık gri arka plan [cite: 50]
+  },
+  content: {
+    padding: 20, // [cite: 51]
   },
 });
